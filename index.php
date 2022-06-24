5578808739da549b09ae8f67d7a23f9cf02abc55
Davide <franc.davi00@gmail.com>
2022-06-24 15:10:25 +0200

    inizio

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+
+    $nome = 'Genoveffa';
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php</title>
+</head>
+<body>
+    <p>Frase originiraria:</p>
+    <h1><p> Oggi è stata una giornata veramente di merda per <?= $nome ?></p></h1>
+</body>
+</html>
